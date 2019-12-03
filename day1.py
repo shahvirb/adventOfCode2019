6decb242433fadae3727b7d8edb061412e251f3d
shahvirb <shahvirb@gmail.com>
2019-12-02 19:01:19 -0600

    initial commit with day 1

---
+++ day1.py
@@ -0,0 +1,42 @@
+import inputreader
+import doctest
+
+
+def calc_fuel(m):
+    """
+    >>> calc_fuel(1969)
+    654
+    >>> calc_fuel(100756)
+    33583
+    """
+    return int(m / 3) - 2
+
+
+def calc_fuel_sum(inputs):
+    return sum([calc_fuel(m) for m in inputs])
+
+
+def calc_fuel_with_child_fuel(m):
+    """
+    >>> calc_fuel_with_child_fuel(14)
+    2
+    >>> calc_fuel_with_child_fuel(1969)
+    966
+    """
+    fuel = calc_fuel(m)
+    return 0 if fuel < 0 else fuel + calc_fuel_with_child_fuel(fuel)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    input = inputreader.readlines('day1.txt')
+
+    # Part 1
+    masses = [int(i) for i in input]
+    print(calc_fuel_sum(masses))
+
+    # Part 2
+    fuels = [calc_fuel_with_child_fuel(m) for m in masses]
+    print(sum(fuels))
---
+++ inputreader.py
@@ -0,0 +1,19 @@
+import os
+import pathlib
+
+BASE_DIR = r'inputs'
+
+def readlines(input = None):
+    if not input:
+        import inspect
+        caller_filename = inspect.stack()[-1][0].f_code.co_filename
+        caller_path = pathlib.Path(caller_filename)
+        input = caller_path.stem + '.txt'
+
+    lines = readfile(os.path.join(BASE_DIR, input))
+    return [l.strip() for l in lines]
+
+
+def readfile(path):
+    with open(path, 'r') as file:
+        return file.readlines()
