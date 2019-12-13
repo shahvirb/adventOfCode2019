b4177a6659f60e30e838b025cf6091e4c021996e
shahvirb <shahvirb@gmail.com>
2019-12-12 19:26:10 -0600

    adding day 6 part 1

--- day6.py
+++ day6.py
@@ -5,9 +5,63 @@ import typing
 @dataclass
 class Node:
     id: str
-    orbiting: typing.Type["Node"]
-    #satellites: typing.List[typing.Type["Node"]] = None
+    orbiting: typing.Type["Node"] = None
+    # satellites: typing.List[typing.Type["Node"]] = None
+
+    def __repr__(self):
+        return self.id
 
 
-if __name__ == '__main__':
-    pass
+def make_nodes(input):
+    """
+    >>> make_nodes("A)B")
+    {'A': A, 'B': B}
+    """
+
+    def get_node(nodes, id):
+        if id not in nodes:
+            nodes[id] = Node(id)
+        return nodes[id]
+
+    all = {}
+    for line in input.splitlines():
+        ids = line.split(")")
+        p = get_node(all, ids[0])
+        s = get_node(all, ids[1])
+        assert not s.orbiting
+        s.orbiting = p
+    return all
+
+
+def distance(node, primary=None):
+    if node.orbiting is None:
+        return 0
+    return 1 + distance(node.orbiting, primary)
+
+
+def count_orbits(nodes, sun_id=None):
+    d = 0
+    i = 0
+    sun = nodes.get(sun_id, None)
+    for id in nodes:
+        total = distance(nodes[id], sun)
+        if total:
+            d += 1
+            i += total - 1
+    return d, i
+
+
+def part1(input):
+    d, i = count_orbits(make_nodes(input), "COM")
+    return d + i
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    import inputreader
+
+    input = inputreader.read("day6.txt")
+    # print(part1("COM)A\nA)B"))
+    print(part1(input))
--- register.py
+++ register.py
@@ -3,6 +3,7 @@ import day2
 import day3
 import day4
 import day5
+import day6
 
 
 REGISTRATION = [
@@ -16,4 +17,5 @@ REGISTRATION = [
     ("2019.4.2", day4.part2),
     ("2019.5.1", day5.part1),
     ("2019.5.2", day5.part2),
+    ("2019.6.1", day6.part1),
 ]
