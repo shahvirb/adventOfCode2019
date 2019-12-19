fd21d967a1e142e34e5da920ae43f3c33de429de
shahvirb <shahvirb@gmail.com>
2019-12-19 08:58:00 -0600

    adding d10p2

--- day10.py
+++ day10.py
@@ -1,5 +1,7 @@
 import collections
 import math
+import itertools
+import operator
 
 Cartesian = collections.namedtuple("Cartesian", ["x", "y"])
 Polar = collections.namedtuple("Polar", ["r", "theta"])
@@ -46,16 +48,24 @@ def test_to_polar():
     assert p.theta == 16.69924423399362
 
 
+def to_cartesian(p, origin=None):
+    def to_int(f):
+        return int(round(f, 10)) # 10 is arbitrary. Test empirically to see if it works.
+
+    x = p.r * math.cos(math.radians(p.theta)) + origin.x if origin else 0
+    y = p.r * math.sin(math.radians(p.theta)) + origin.y if origin else 0
+    # print(x,y)
+    return Cartesian(to_int(x), to_int(y))
+
+
 def make_objects(input):
     asteroids = []
-    empty = [] #TODO why do the empties matter?
-    containers = {"#": asteroids, ".": empty}
     for y, line in enumerate(input.splitlines()):
         for x, ch in enumerate(line):
-            assert ch in containers
-            containers[ch].append(Cartesian(x, y))
-    assert (x + 1) * (y + 1) == len(asteroids) + len(empty)
-    return asteroids, empty
+            assert ch == '#' or ch == '.'
+            if ch == '#':
+                asteroids.append(Cartesian(x, y))
+    return asteroids
 
 
 def angles(origin, asteroids):
@@ -70,9 +80,37 @@ def angles(origin, asteroids):
 
 
 def part1(input):
-    asteroids, empties = make_objects(input)
+    asteroids = make_objects(input)
     counts = [(len(angles(a, asteroids).keys()), a) for a in asteroids]
     return max(counts)
+
+
+def part2(input, stop=200):
+    asteroids = make_objects(input)
+    counts = [(len(angles(a, asteroids).keys()), a) for a in asteroids]
+    count, best = max(counts)
+
+    asteroid_angles = angles(best, asteroids)
+    # Sort the asteroids by r so that they're vaporized from closest outwards
+    for a in asteroid_angles:
+        asteroid_angles[a] = sorted(asteroid_angles[a], key=operator.attrgetter('r'))
+
+
+    all_angles = sorted(asteroid_angles.keys(), reverse = False)
+    start_i = all_angles.index(270.0)
+    angle_it = itertools.islice(itertools.cycle(all_angles), start_i, None)
+
+    hits = 0
+
+    while hits < stop:
+        angle = next(angle_it)
+        if len(asteroid_angles[angle]) > 0:
+            hit_asteroid = asteroid_angles[angle].pop(0)
+            hits += 1
+            # print(f"{hits} best={best} hit_asteroid={hit_asteroid} {to_cartesian(hit_asteroid, best)}")
+
+    hit_cart = to_cartesian(hit_asteroid, best)
+    return hit_cart.x * 100 + hit_cart.y
 
 
 if __name__ == "__main__":
@@ -80,3 +118,4 @@ if __name__ == "__main__":
 
     input = inputreader.read("day10.txt")
     print(part1(input))
+    print(part2(input, 200))
